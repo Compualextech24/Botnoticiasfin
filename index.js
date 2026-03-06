e01384116b05ac0b63e07116a58185c4815fbe08
Compualextech24 <compualextech24@gmail.com>
2026-03-06 02:18:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ const firstTimeUsers    = new Set();
 const MAX_CACHE_SIZE = 500;
 const NEWS_GROUP_ID  = "120363371012169967@g.us";
 const NEWS_SCHEDULE  = [
-    { hour: 22, minute: 30 },
-    { hour: 2, minute: 10 }
+    { hour: 22, minute: 00 },
+    { hour: 10, minute: 00 }
 ];
 
 let lastNewsSentKey  = null;
@@ -968,3 +968,4 @@ async function connectToWhatsApp() {
 console.log("Iniciando Ghost Bot...");
 console.log(`Zona horaria: ${process.env.TZ || 'America/Mexico_City'}`);
 connectToWhatsApp();
+
